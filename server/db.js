254ae0c9900075a0f54f66f713dedb6f04dfd1e4
Aqil <aqilsheraz2010@gmail.com>
2022-05-24 22:07:05 +0500

    todo

--- server/db.js
+++ server/db.js
@@ -1,9 +1,16 @@
 const Pool = require("pg").Pool;
 
+// const pool = new Pool({
+//   user: "aqil1",
+//   password: "aqil1234",
+//   host: "localhost",
+//   port: 5432,
+//   database: "perntodo"
+// });
 const pool = new Pool({
-  user: "aqil1",
-  password: "aqil1234",
-  host: "localhost",
+  user: "aqil",
+  password: "aqil12345678",
+  host: "my-db-instance.cwk9vigvhmvi.us-east-1.rds.amazonaws.com",
   port: 5432,
   database: "perntodo"
 });
